075d49331fc9b03e2ee47f0d673f841a51aae6aa
Cesar A Medina <medin297@umn.edu>
2026-05-08 14:00:24 -0500

    Create 04_pipeline_works.sh

---
+++ 05_UG_scripts/04_pipeline_works.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Define Paths
+
+INPUT_CRAM="/90daydata/xu_alfalfabreeding/system_from_home/msi/UG100/02_files/421685-S18-NGX-Z0018-CATGCGTCCTGTGAT.cram"
+REF="/90daydata/xu_alfalfabreeding/system_from_home/msi/UG100/02_files/haplo_01_mem2/haplo_01.fa"
+OUT_DIR="/90daydata/xu_alfalfabreeding/system_from_home/msi/UG100/06_output"
+BIND_PATH="/project/xu_alfalfabreeding/system_from_home/msi/UG100/05_UG_scripts"
+
+# Step 1: Indexing
+# UA indexing is slow and creates a ~50GB .uai file
+# works ok
+apptainer run alignment.sif ua --build=true --ref $REF --index $REF.uai
+
+# work ok
+apptainer run alignment.sif ua --build --ref $REF --index  $REF.uai --progress
+
+# Step 2 : Align with UA
+# When working with a single input BAM/CRAM file
+# work ok
+samtools view -h -@ 32 $INPUT_CRAM -T $REF -F 2048 | \
+apptainer run alignment.sif ua \
+    --index $REF.uai \
+    --align true \
+    --progress \
+    --tp reference \
+    --stat=$OUT_DIR/output_basename.%s.json \
+    --nthread max \
+    --sam-input - \
+    --sam-output - \
+    --seed-score-ratio 0.5 --vector --soft-clipping | \
+samtools view -@ 32 -o $OUT_DIR/output_basename.bam -
+
+# Step 3 : Sort UA Aligned BAM with Demux and Sorter
+# The 'demux' tool prepares the BAM for the sorter. 
+# Use --channel-id=0 to prevent the 'fail to scan' error you encountered.
+# work ok
+samtools view -h -@ 32 $OUT_DIR/output_basename.bam | \
+apptainer run sorter.sif demux \
+    --input=- \
+    --output-dir=$OUT_DIR/ \
+    --runid=output_basename \
+    --nthreads 32 \
+    --progress \
+    --reference ${REF} \
+    --mark-duplicates=true \
+    --output-group=output_basename \
+    --output-path={outputGroup}/{outputGroup} \
+    --align=true \
+    --progress \
+    --cram-ignore-md5 \
+    --channel-id 0
+
+# Step 3.1: Sorter Command
+apptainer run sorter.sif sorter \
+    --runid=output_basename \
+    --input-dir=$OUT_DIR/ \
+    --output-dir=$OUT_DIR/ \
+    --nthreads 32 \
+    --progress \
+    --timestamp=000  # Used to override sorter output timestamp
